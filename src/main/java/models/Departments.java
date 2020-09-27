a422fbcf1ac1c3ac2d6712a867f1524587af094a
wil-bert <ianwilbuts@gmail.com>
2020-09-27 18:21:37 +0300

    class constructors

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,5 +1,17 @@
 package models;
 
 public class Departments {
+    private String departmentName;
+    private String description;
+    private int numberOfEmployees;
+    private int departmentId;
+
+    public Departments(String departmentName,String description, int numberOfEmployees, int departmentId){
+        this.departmentName = departmentName;
+        this.description = description;
+        this.numberOfEmployees = numberOfEmployees;
+        this.departmentId = departmentId;
+    }
+
 
 }
--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,13 @@
 package models;
 
 public class News {
+    private String generalNews;
+    private String departmentNews;
+    private int id;
+
+    public News(String generalNews, String departmentNews){
+        this.generalNews = generalNews;
+        this.departmentNews = departmentNews;
+    }
+
 }
