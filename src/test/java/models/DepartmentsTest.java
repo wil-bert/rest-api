e0b13d0b440cf825a59c6f232690d754f4bfc165
wil-bert <ianwilbuts@gmail.com>
2020-09-27 20:43:23 +0300

    department test run

--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -16,4 +16,32 @@ public class DepartmentsTest {
     public void teardown() throws Exception {
     }
 
+    @Test
+    public void getDepartmentNameReturnsDepartmentName() throws Exception {
+        Departments testDepartments = setupDepartments();
+        assertEquals("ICT", testDepartments.getDepartmentName());
+    }
+
+    @Test
+    public void getDescriptionReturnsDescription() throws Exception {
+        Departments testDepartments = setupDepartments();
+        assertEquals("Solves all technical issues about computers", testDepartments.getDescription());
+    }
+
+    @Test
+    public void getNumberOfEmployeesReturnsNumberOfEmployees() throws Exception {
+        Departments testDepartments = setupDepartments();
+        assertEquals( 5, testDepartments.getNumberOfEmployees());
+    }
+
+    @Test
+    public void getDepartmentIdReturnsDepartmentId() throws Exception {
+        Departments testDepartments = setupDepartments();
+        assertEquals(1, testDepartments.getDepartmentId());
+    }
+
+    private Departments setupDepartments() {
+        return new Departments("ICT", "Solves all technical issues about computers", 5, 1);
+    }
+
 }
