8d49e07c88f97a0e261b9cf9b1fe6f53169c6730
wil-bert <ianwilbuts@gmail.com>
2020-09-27 19:06:06 +0300

    edit user test

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -22,6 +22,26 @@ public class UserTest {
         assertEquals("Ian Wilbert", testUser.getName());
     }
 
+    @Test
+    public void getPositionReturnsCorrectPosition() throws Exception{
+        User testUser = setupUser();
+        assertEquals("Employee", testUser.getPosition());
+    }
+
+    @Test
+    public void getRoleReturnsCorrectRole() throws Exception{
+        User testUser = setupUser();
+        assertEquals("Tech", testUser.getRole());
+    }
+
+    @Test
+    public void getDepartmentReturnsCorrectDepartment() throws Exception{
+        User testUser = setupUser();
+        assertEquals("ICT", testUser.getDepartment());
+    }
+
+
+
     private User setupUser() {
         return new User("Ian Wilbert","Employee", "Tech", "ICT");
     }
