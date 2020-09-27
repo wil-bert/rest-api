a4e0eb7b365b2adfaa023c0af61fb27ad6922db9
wil-bert <ianwilbuts@gmail.com>
2020-09-27 18:11:00 +0300

    edit user details

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,16 @@
 package models;
 
 public class User {
+    private String name;
+    private String position;
+    private String role;
+    private String department;
+    private int id;
+
+    public User(String name, String position, String role, String department){
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.department = department;
+    }
 }
