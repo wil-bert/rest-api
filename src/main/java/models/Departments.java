55e22821c390d5385367ba53da0b66320a323a30
wil-bert <ianwilbuts@gmail.com>
2020-09-27 19:23:22 +0300

    update department getter

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -5,12 +5,33 @@ public class Departments {
     private String description;
     private int numberOfEmployees;
     private int departmentId;
+    private int id;
 
     public Departments(String departmentName,String description, int numberOfEmployees, int departmentId){
         this.departmentName = departmentName;
         this.description = description;
         this.numberOfEmployees = numberOfEmployees;
         this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public int getId() {
+        return id;
     }
 
 
