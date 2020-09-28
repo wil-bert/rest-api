8f1b45add1f9d076ce21e86ae087757740aac6a8
wil-bert <ianwilbuts@gmail.com>
2020-09-28 08:26:32 +0300

    refactor news and department dao

--- src/test/java/dao/SqlUsersDaoTest.java
+++ src/test/java/dao/SqlUsersDaoTest.java
@@ -1,7 +1,18 @@
 package dao;
 
+import models.User;
+import models.Departments;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Before;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 import static org.junit.Assert.*;
 
 public class SqlUsersDaoTest {
+    private Connection conn;
+    private Sql2oUsersDao usersDao;
+    private Sql2oDepartmentsDao departmentDao;
 
 }
