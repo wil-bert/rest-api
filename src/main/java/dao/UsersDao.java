420ec30fa19a7afc9ec4aa0378a02dcc703d6b7e
wil-bert <ianwilbuts@gmail.com>
2020-09-29 07:33:52 +0300

    edit usersDao

--- src/main/java/dao/UsersDao.java
+++ src/main/java/dao/UsersDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import models.Departments;
 import models.User;
 
 import java.util.List;
@@ -9,7 +10,7 @@ public interface UsersDao {
     void add(User user);
 
     List<User> getAll();
-    List<User> getAllUsersByDepartment(int department);
+    List<Departments> getAllUsersByDepartment(int department);
 
     void deleteById(int id);
     void clearAll();
